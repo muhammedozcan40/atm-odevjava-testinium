80524b143d055bf9ad5bca4575beb143cc6b401f
muhammedozcan40 <54441202+muhammedozcan40@users.noreply.github.com>
2021-06-22 10:49:33 +0300

    duzenleme

--- src/main/java/com/testinium/atmodev/Atm.java
+++ src/main/java/com/testinium/atmodev/Atm.java
@@ -6,13 +6,13 @@ import com.testinium.atmodev.musteriler.Musteriler;
 
 public class Atm {
 
-    // TODO: 22.06.2021  Bu uygulamada oluşturulan müşteri numaraları 100001,100002,100003,10004,10005 şeklindedir
-    // TODO: 22.06.2021   Virgüllü bakiye girilebilir.
-     
+   /*  Bu uygulamada oluşturulan müşteri numaraları 100001,100002,100003,10004,10005 şeklindedir
+      Virgüllü bakiye girilebilmektedir.
+    */
 
     public static void main(String[] args) {
         try {
-            
+
             System.out.println("Ozcan Bank'a hoşgeldiniz...\n Lütfen müşteri numarası giriniz");
             String musteriNo = MusteriBilgileriGir.musteriNoGir();
             BakiyeIslemleri.islemGerceklestir(Musteriler.musteriListesi().get(musteriNo));
